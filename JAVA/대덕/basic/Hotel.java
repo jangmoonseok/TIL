cc558619cb3178a4ca7987ca7e7503abab1cc2a4
jangmoonseok <64582209+jangmoonseok@users.noreply.github.com>
2022-02-25 16:45:14 +0900

    Add files via upload

--- JAVA/대덕/basic/Hotel.java
+++ JAVA/대덕/basic/Hotel.java
@@ -5,12 +5,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Hotel {
 	
 	private static Map<Integer, Room> roomMap = new HashMap<Integer, Room>();
-	
+	private Scanner s = new Scanner(System.in);
 	public static void main(String[] args) {
 		int single = 201;
 		int dob = 301;
@@ -25,21 +26,74 @@ public class Hotel {
 			dob++;
 			sweet++;
 		}
+		
+		new Hotel().start();
+	}
 
+	private void start() {
+		while(true) {			
+			System.out.println("==================================");
+			System.out.println("\t호텔문을 열었습니다. 어서오세요.");
+			System.out.println("==================================");
+			System.out.println();
+			System.out.println();
+			System.out.println("----------------------------------");
+			System.out.println("어떤 업무를 하시겠습니까?");
+			System.out.println("1.체크인  2.체크아웃  3.객실상태  4.업무종료");
+			System.out.println("----------------------------------");
+			System.out.print("하실 업무를 입력해주세요 > ");
+			int input = s.nextInt();
+			
+			switch(input) {
+			case 1: checkIn(); break;
+			case 2: checkOut(); break;
+			case 3: viewRoom(); break;
+			case 4: 
+				System.out.println("==================================");
+				System.out.println("\t호텔문을 닫았습니다.");
+				System.out.println("==================================");
+				System.exit(0);
+			}
+		}
+	}
+	
+	private void checkOut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void checkIn() {
+		s.nextLine();
+		System.out.println("------------------------------");
+		System.out.println("체크인 작업");
+		System.out.println("------------------------------");
+		System.out.println("201~209 : 싱글룸");
+		System.out.println("301~309 : 더블룸");
+		System.out.println("401~409 : 스위트룸");
+		System.out.println("------------------------------");
+		System.out.print("방 번호 입력 => ");
+		int roomNum = s.nextInt();
+		if(roomMap.get(roomNum).getUser() != "-") {
+			System.out.println(roomNum + "호 객실은 이미 손님이 있습니다.");
+		}else {
+			System.out.print("체크인 하실 고객명을 입력해주세요 > ");
+			String user = s.nextLine();
+			roomMap.get(roomNum).setUser(user);
+			System.out.println(user + "님 체크인이 완료되었습니다.");
+		}
+	}
+
+	private void viewRoom() {
 		Set<Integer> roomSet = roomMap.keySet();
 		List<Integer> roomList = new ArrayList<>(roomSet);
 		Collections.sort(roomList);
-		System.out.println(roomList);
+		
 		System.out.println("방 번호\t방 종류\t투숙객");
 		for(Integer roomNum : roomList) {
 			Room room = roomMap.get(roomNum);
 			System.out.println(room.num + "\t" + room.name + "\t" + room.user);
 			System.out.println("-------------------------------");
 		}
-	}
-
-	private void start() {
-		
 	}
 
 }
@@ -54,4 +108,14 @@ class Room{
 		this.num = num;
 		this.name = name;
 	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	
 }
