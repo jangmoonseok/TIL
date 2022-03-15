b3d1330272e264b247e2012758bdd445e07c71d8
jangmoonseok <64582209+jangmoonseok@users.noreply.github.com>
2022-03-15 11:06:36 +0900

    Add files via upload

--- JAVA/대덕/NetWork/TCPFileClient.java
+++ JAVA/대덕/NetWork/TCPFileClient.java
@@ -7,6 +7,13 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/*
+	서버와 클라이언트가 접속이 이루어지면 클라이언트가 d:/d_other/펭귄.jpg파일을 서버로 전송하면 서버는
+	이 데이터를 받아서 d:/d_other/down폴더에 펭귄_전송본.jpg로 저장한다.
+	
+	서버 : 소켓으로 입력 받아서 파일로 출력
+	클라이언트 : 파일로 입력 받아서 소켓으로 출력
+*/
 public class TCPFileClient {
 
 	public static void main(String[] args) throws IOException {
@@ -14,11 +21,12 @@ public class TCPFileClient {
 		System.out.println("서버에 연결되었습니다...");
 		System.out.println();
 		
-		FileInputStream fin = new FileInputStream("d:/d_other/펭귄.jpg");
-		OutputStream out = socket.getOutputStream();
+		FileInputStream fin = new FileInputStream("d:/d_other/펭귄.jpg"); // 파일을 입력받아야 하기 때문에 FileInputStream사용
+		OutputStream out = socket.getOutputStream(); // 소켓을 통해 서버로 전달해야하기 때문에 socket.getOutputStream()사용
 		
 		int data = 0;
-		while ( (data = fin.read()) != -1 ) {
+		
+		while ( (data = fin.read()) != -1 ) { // 입력 받은 파일을 소켓으로 출력
 			out.write(data);
 		}
 		
--- JAVA/대덕/NetWork/TCPFileServer.java
+++ JAVA/대덕/NetWork/TCPFileServer.java
@@ -12,6 +12,9 @@ import java.net.Socket;
 /*
  	서버와 클라이언트가 접속이 이루어지면 클라이언트가 d:/d_other/펭귄.jpg파일을 서버로 전송하면 서버는
  	이 데이터를 받아서 d:/d_other/down폴더에 펭귄_전송본.jpg로 저장한다.
+ 	
+ 	서버 : 소켓으로 입력 받아서 파일로 출력
+ 	클라이언트 : 파일로 입력 받아서 소켓으로 출력
  */
 public class TCPFileServer {
 
@@ -25,11 +28,11 @@ public class TCPFileServer {
 		System.out.println();
 
 		
-		InputStream is = socket.getInputStream();
-		FileOutputStream fout = new FileOutputStream("d:/d_other/down/펭귄_전송본.jpg");
+		InputStream is = socket.getInputStream(); // 소켓으로 입력을 받아야하기 때문에 socket.getInputStream()사용
+		FileOutputStream fout = new FileOutputStream("d:/d_other/down/펭귄_전송본.jpg"); // 파일로 출력해야하기 때문에 FileOutputStream()사용
 		
 		int data = 0;
-		while((data = is.read()) != -1) {
+		while((data = is.read()) != -1) { // 소켓으로 입력받은 데이터를 파일로 출력
 			fout.write(data);
 		}
 		
