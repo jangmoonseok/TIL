9d2540c3cdcca37f61053c34d943706a077d6fec
jangmoonseok <cacao621@naver.com>
2024-01-09 00:23:41 +0900

    2024/01/09 Generic TypeErasure Practice2

--- Java_Generic/src/gnr/example/TypeErasureExample.java
+++ Java_Generic/src/gnr/example/TypeErasureExample.java
@@ -46,16 +46,26 @@ public class TypeErasureExample<T extends Number> {
 // 치환 후
 class TypeErasureExample1{
 
+	/*
+	 * List<T>가 에서 T는 Object로 치환
+	 */
 	private List list;
 
 	public TypeErasureExample1() {
-		list = new ArrayList();
+		list = new ArrayList(); // 제네릭 타입<T> 제거
 	}
 	
+	/*
+	 * 매개변수 T는 <T extends Number>의 하위이므로 Number로 치환
+	 */
 	void add(Number element) {
 		list.add(element);
 	}
 	
+	/*
+	 * 반환타입 T는 <T extends Number>의 하위이므로 Number로 치환
+	 * list.get()은 Object타입으로 반환하므로 Number으로 형 변환
+	 */
 	Number getValue(int i) {
 		return (Number)list.get(i);
 	}
